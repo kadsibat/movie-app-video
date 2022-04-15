bbbb3404125d616417a001d87df5d1257a8e91f2
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-15 12:12:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AuthContextProvider from "./context/AuthContext";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <AuthContextProvider>
+      <AppRouter/>
+    </AuthContextProvider>
+  );
+};
+
+export default App;
---
+++ src/router/AppRouter.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Navbar from "../components/Navbar";
+import MovieDetail from "../pages/MovieDetail";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details" element={<MovieDetail />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
